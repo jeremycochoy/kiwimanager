7f17d18239d51692011ef79cb2f6680ff77dc6bf
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2013-06-05 16:45:23 +0200

    Initial project

---
+++ snaplets/heist/templates/index.tpl
@@ -0,0 +1,3 @@
+<apply template="base">
+  <h2>KiwiMonitor</h2>
+</apply>
---
+++ src/Site.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+------------------------------------------------------------------------------
+-- | This module is where all the routes and handlers are defined for your
+-- site. The 'app' function is the initializer that combines everything
+-- together and is exported by this module.
+module Site
+  ( app
+  ) where
+
+------------------------------------------------------------------------------
+import           Data.ByteString (ByteString)
+import           Snap.Snaplet
+import           Snap.Snaplet.Heist
+import           Snap.Util.FileServe
+import           Heist
+------------------------------------------------------------------------------
+import           Application
+
+
+------------------------------------------------------------------------------
+-- | The application's routes.
+routes :: [(ByteString, Handler App App ())]
+routes = [ ("",          serveDirectory "static")
+         ]
+
+
+------------------------------------------------------------------------------
+-- | The application initializer.
+app :: SnapletInit App App
+app = makeSnaplet "app" "KiwiMonitor application." Nothing $ do
+    h <- nestSnaplet "" heist $ heistInit "templates"
+    addRoutes routes
+    return $ App h
+
